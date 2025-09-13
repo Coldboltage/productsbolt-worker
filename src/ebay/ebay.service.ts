f714c7dcd210464e4cf4de7fe1a5cd5be5ef0ec9
Coldboltage <alandreid@hotmail.co.uk>
2025-09-14 00:48:58 +0100

    feat: add ebay-oauth-nodejs-client for improved eBay API authentication and update EbayService methods

--- src/ebay/ebay.service.ts
+++ src/ebay/ebay.service.ts
@@ -4,12 +4,14 @@ import { CreateEbayDto } from './dto/create-ebay.dto.js';
 import { UpdateEbayDto } from './dto/update-ebay.dto.js';
 import 'dotenv/config';
 import { EbayTokenInterface } from './entities/ebay.entity.js';
+import EbayAuthToken from 'ebay-oauth-nodejs-client';
+
 
 @Injectable()
 export class EbayService implements OnModuleInit {
-   async onModuleInit() {
+  async onModuleInit() {
     try {
-      const test = await this.getUrlsFromApiCall('Magic The Gathering Final Fantasy Collector', 'It is an old amp, from the 1960s', 'AMP')
+      const test = await this.getUrlsFromApiCall('Magic: The Gathering MTG - Final Fantasy Collector Booster Box', 'Magic: The Gathering MTG - Final Fantasy Collector Booster Box', 'TCG')
       console.log(test)
     } catch (error) {
       console.log(error)
@@ -24,9 +26,15 @@ export class EbayService implements OnModuleInit {
   }
 
   async getUrlsFromApiCall(productName: string, productContext: string, productType: string): Promise<ProductInStockWithAnalysisStripped[]> {
-    const apiKey = await this.getApiKey(process.env.EBAY_APPID, process.env.EBAY_CERTID)
-    console.log(apiKey)
-    const productEpidList = await fetch(`https://api.ebay.com/commerce/catalog/v1_beta/product_summary/search?q=${encodeURIComponent(productName)}`, {
+    console.log({
+      appid: process.env.EBAY_APPID,
+      certId: process.env.EBAY_CERTID
+    })
+    // const apiKey = await this.getApiKey(process.env.EBAY_APPID, process.env.EBAY_CERTID)
+    const apiKey = {
+      access_token: process.env.TEMP_EBAY_KEY
+    }
+    const productEpidList = await fetch(`https://api.ebay.com/buy/browse/v1/item_summary/search?q=${encodeURIComponent(productName)}`, {
       method: "GET",
       headers: {
         // 'Authorization': `Bearer ${process.env.EBAY_API_KEY}`, // Use the API key from the environment variable
@@ -35,14 +43,37 @@ export class EbayService implements OnModuleInit {
       }
     })
     const json = await productEpidList.json()
-    console.log(json)
-    // Loop through all of the epIds and get a description of each epid.
-    // Use OpenAI nano and sanity check for the best epid
-    // Loop through all listings of best epId
-    // Use the existing openai search thing I have
-    // create a list of correct listings
+    // console.log(json)
 
-    // NOTE. We don't want to nano anything already checked. 
+    const test = await fetch("https://api.ebay.com/buy/browse/v1/item_summary/search?q=Innistrad+Remastered+Play+Booster+Box&X-EBAY-C-MARKETPLACE-ID=EBAY_GB", {
+      headers: {
+        // 'Authorization': `Bearer ${process.env.EBAY_API_KEY}`, // Use the API key from the environment variable
+        'Authorization': `Bearer ${apiKey.access_token}`, // Use the API key from the environment variable
+        'Content-Type': 'application/json',
+        "X-EBAY-C-MARKETPLACE-ID": "EBAY_GB"
+
+      }
+    })
+    const epidTest = await test.json()
+
+    // console.log(epidTest)
+
+
+    const stripedProducts = []
+
+    for (const product of epidTest.itemSummaries) {
+      const strippedItem = {
+        name: product.title,
+        price: {
+          value: product.price.value,
+          currency: product.price.currency
+        }
+      }
+
+      stripedProducts.push(strippedItem)
+    }
+
+    console.log(stripedProducts)
     return Promise.resolve([{
       analysis: "Looks genuine and available.",
       inStock: true,
@@ -50,26 +81,23 @@ export class EbayService implements OnModuleInit {
       currencyCode: "EUR",
       conciseReason: "Verified in stock from supplier website.",
       specificUrl: "https://example.com/product/123",
-    } ] )
+    }])
   }
 
-  async getApiKey(appId: string, certId: string): Promise<EbayTokenInterface> {
-    const credentials =  Buffer.from(`${appId}:${certId}`).toString('base64')
-    const refreshToken = 'v^1.1#i^1#r^1#I^3#f^0#p^3#t^Ul4xMF8yOjQ3MTY2NThBMDNENDVBM0RCNTYxNjExOTUzNkQ5QzBBXzFfMSNFXjI2MA==';
-    const response = await fetch('https://api.ebay.com/identity/v1/oauth2/token', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": `Basic ${credentials}`
-      },
-      body: new URLSearchParams({
-        grant_type: 'refresh_token',
-        refresh_token: refreshToken,
-        scope: 'https://api.ebay.com/oauth/api_scope/sell.inventory'
-      })
+  async getApiKey(appId: string, certId: string) {
+    const credentials = Buffer.from(`${appId.trim()}:${certId.trim()}`).toString('base64')
+    console.log(credentials)
+
+    const ebayAuthToken = new EbayAuthToken({
+      clientId: process.env.EBAY_APPID,
+      clientSecret: process.env.EBAY_CERTID,
+      redirectUri: 'Alan_Reid-AlanReid-Produc-ycbszzu' // from your Dev Portal RuName
     });
-    const json: EbayTokenInterface = await response.json()
-    return json
+
+    const appToken = await ebayAuthToken.getApplicationToken('PRODUCTION');
+    console.log(appToken);
+
+    return ebayAuthToken
   }
 
   findOne(id: number) {
